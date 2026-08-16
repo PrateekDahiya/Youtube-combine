bb773527eba444570c0091a4e7841ab2ba4a8797
Prateek Dahiya <dahiyaprateek27@gmail.com>
2026-08-17 00:44:28 +0530

    refactor: use InnerTube getAttestationChallenge instead of JSDOM page scraping

--- server/src/youtube/poTokenGenerator.js
+++ server/src/youtube/poTokenGenerator.js
@@ -1,8 +1,7 @@
-const { Innertube, Platform, Types, UniversalCache } = require("youtubei.js");
+const { Innertube, Platform, UniversalCache } = require("youtubei.js");
 const { BotGuardClient } = require("bgutils-js/botguard");
 const { WebPoMinter } = require("bgutils-js/webpo");
-const { buildURL, getHeaders, parseLooseJSON } = require("bgutils-js/utils");
-const { JSDOM } = require("jsdom");
+const { buildURL, getHeaders } = require("bgutils-js/utils");
 const vm = require("vm");
 
 Platform.shim.eval = async (data) => {
@@ -14,51 +13,12 @@ let poMinterExpiry = 0;
 const PO_TOKEN_TTL_BUFFER = 60000;
 
 async function initializePoMinter() {
-    const dom = new JSDOM('<!DOCTYPE html><html lang="en"><head><title></title></head><body></body></html>', {
-        url: 'https://www.youtube.com',
-        referrer: 'https://www.youtube.com/',
-        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36(KHTML, like Gecko)',
-    });
+    console.log("[PO Token] Initializing PO token minter...");
+    const innertube = await Innertube.create({ cache: new UniversalCache(true) });
 
-    const pageResponse = await fetch('https://www.youtube.com', {
-        headers: {
-            "accept": "*/*",
-            "accept-language": "en-US,en;q=0.7",
-            "user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36(KHTML, like Gecko)',
-        }
-    });
-
-    const pageHtml = await pageResponse.text();
-
-    const ytConfig = pageHtml.match(/ytcfg\.set\(({.+?})\);/s)?.[1];
-    if (!ytConfig) {
-        throw new Error('Could not find ytcfg in page HTML');
-    }
-
-    dom.window.yt = { config_: JSON.parse(ytConfig) };
-
-    Object.assign(globalThis, {
-        yt: dom.window.yt,
-        window: dom.window,
-        document: dom.window.document,
-        location: dom.window.location,
-        origin: dom.window.origin
-    });
-
-    if (!('navigator' in globalThis)) {
-        Object.defineProperty(globalThis, 'navigator', { value: dom.window.navigator });
-    }
-
-    const initialAttestationData = pageHtml.match(/window\.ytAtN\(\s*({[\s\S]*?})\s*\)/);
-    if (!initialAttestationData) {
-        throw new Error('Could not find challenge in page HTML');
-    }
-
-    const initialAttestationDataJson = parseLooseJSON(initialAttestationData[1]);
-    const challengeResponse = initialAttestationDataJson.R;
-
+    const challengeResponse = await innertube.getAttestationChallenge('ENGAGEMENT_TYPE_UNBOUND');
     if (!challengeResponse.bgChallenge) {
-        throw new Error('Could not get challenge');
+        throw new Error('Could not get attestation challenge from InnerTube');
     }
 
     const interpreterUrl = challengeResponse.bgChallenge.interpreterUrl.privateDoNotAccessOrElseTrustedResourceUrlWrappedValue;
@@ -100,11 +60,17 @@ async function initializePoMinter() {
 
     poMinter = await WebPoMinter.create(integrityTokenData, webPoSignalOutput);
     poMinterExpiry = Date.now() + (estimatedTtlSecs * 1000) - PO_TOKEN_TTL_BUFFER;
+    console.log("[PO Token] PO token minter initialized, expires in " + estimatedTtlSecs + "s");
 }
 
 async function getPoToken(videoId) {
     if (!poMinter || Date.now() >= poMinterExpiry) {
-        await initializePoMinter();
+        try {
+            await initializePoMinter();
+        } catch (error) {
+            console.error("[PO Token] Failed to initialize: " + error.message);
+            throw error;
+        }
     }
     return await poMinter.mintAsWebsafeString(videoId);
 }
